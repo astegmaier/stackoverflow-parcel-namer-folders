71cf2e8273a67e1f04288dedef7dc99fba800bc6
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-10-12 12:01:22 -0400

    Implement folder namer.

--- parcel-namer-folders/src/FoldersNamer.ts
+++ parcel-namer-folders/src/FoldersNamer.ts
@@ -1,7 +1,29 @@
 import { Namer } from "@parcel/plugin";
+import type { Bundle } from "@parcel/types";
+import path from "path";
 
 export default new Namer({
   name({ bundle }) {
-    return null;
+    switch (bundle.type) {
+      case "js":
+        return getPathWithFolder("scripts", bundle);
+      case "less":
+        return getPathWithFolder("layout", bundle);
+      case "scss":
+        return getPathWithFolder("layout", bundle);
+      default:
+        return null;
+    }
   },
 });
+
+function getPathWithFolder(folderName: string, bundle: Bundle): string | null {
+  const filePath = bundle.getMainEntry()?.filePath;
+  if (!filePath) return null;
+  let nameWithoutExtension = path.basename(filePath, path.extname(filePath));
+  if (!bundle.needsStableName) {
+    // See: https://parceljs.org/plugin-system/namer/#content-hashing
+    nameWithoutExtension += "." + bundle.hashReference;
+  }
+  return `${folderName}/${nameWithoutExtension}.${bundle.type}`;
+}
